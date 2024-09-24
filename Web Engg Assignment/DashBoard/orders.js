11ca1e05c6c52e6b94725e4afbe8297191e833c2
Dipa Lahery <130212365+dipa1971@users.noreply.github.com>
2024-09-24 21:16:25 +0600

    Add files via upload

---
+++ Web Engg Assignment/DashBoard/orders.js
@@ -0,0 +1,47 @@
+const Orders = [
+
+    {
+        productName: 'Foldable mini drone',
+        productNumber: '85631',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '37899',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko Fu Pro Drone',
+        productNumber: '93497',
+        paymentStatus: 'Due', 
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996', 
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821', 
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S', 
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482', 
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+
+]
+
